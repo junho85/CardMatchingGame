b9be8c9c2ce8ef2e7ab3280b0b534bc3800fd69a
june.kim <junho85@gmail.com>
2016-01-21 20:57:33 +0900

    init

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.post('/', function (req, res) {
+    res.send('Got a POST request');
+});
+
+app.put('/user', function (req, res) {
+    res.send('Got a PUT request at /user');
+});
+
+app.delete('/user', function (req, res) {
+    res.send('Got a DELETE request at /user');
+});
+
+app.use(function(req, res, next) {
+    res.status(404).send('Sorry cant find that!');
+});
+
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
